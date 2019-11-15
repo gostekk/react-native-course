54a9703eb56da464836e29188d64409716713160
gostekk <gjaworski@protonmail.com>
2019-11-15 22:18:38 +0100

    End of section 6

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -3,17 +3,32 @@ import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
   const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <View>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        value={name}
-        onChangeText={(newValue) => setName(newValue)}
-      />
-      <Text>My name is: {name}</Text>
+      <View>
+        <Text>Enter Name:</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={name}
+          onChangeText={(newValue) => setName(newValue)}
+        />
+        <Text>My name is {name}</Text>
+      </View>
+      <View>
+        <Text>Enter Password:</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={password}
+          onChangeText={(newValue) => setPassword(newValue)}
+        />
+        { password.length < 5 ? <Text style={styles.errorText}>Password must be longer than 5 characters</Text> : undefined }
+      </View>
     </View>
   );
 };
@@ -23,6 +38,9 @@ const styles = StyleSheet.create({
     margin: 15,
     borderColor: 'black',
     borderWidth: 1,
+  },
+  errorText: {
+    color: 'red',
   }
 });
 
